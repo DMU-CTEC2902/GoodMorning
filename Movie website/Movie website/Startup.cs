1f461f4152d7444eff21d84877003d7d6a039354
Bin-A <Bin-A@LAPTOP-2G2915GD>
2019-02-18 12:43:23 +0000

    New project

---
+++ Movie website/Movie website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Movie_website.Startup))]
+namespace Movie_website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
