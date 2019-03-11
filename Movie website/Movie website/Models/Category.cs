cd211a0576b4ac2dc5db35a2aee8c66723542bdf
Bin-A <Bin-A@LAPTOP-2G2915GD>
2019-03-11 00:54:19 +0000

    Category and films classes done

---
+++ Movie website/Movie website/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Movie_website.Models
+{
+    public class Category
+    {
+        public virtual int CategoryId { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Description { get; set; }
+
+    }
+}
---
+++ Movie website/Movie website/Models/Films.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Movie_website.Models
+{
+    public class Films
+    {
+        public virtual int FilmId { get; set; }
+        public virtual int CategoryId { get; set; }
+        public virtual string FilmName { get; set; }
+        public virtual string Description { get; set; }
+        public virtual DateTime DateReleased { get; set; }
+        public virtual string Language { get; set; }
+        public virtual Category Category { get; set; }
+
+    }
+}
